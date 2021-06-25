69bfc6556a9d3555c8cba1ab0f92cb66fa7d7981
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-06-25 21:43:10 +0530

    snake functions added

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import turtle as t
+from snake import Snake
+import time
 
 screen = t.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snack Game V1")
+screen.tracer(0)
 
+snake = Snake()
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
+    snake.move()
 
 screen.exitonclick()
