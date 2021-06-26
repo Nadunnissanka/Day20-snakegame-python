8dfefab2bdbdab5ebf74626b23e56929746a7e5d
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-06-26 16:31:46 +0530

    Snake game functionalty added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle as t
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = t.Screen()
@@ -9,6 +11,8 @@ screen.title("Snack Game V1")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,7 +24,13 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detecting Collation
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+
+
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.score = 0
+        self.goto(0, 270)
+        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.hideturtle()
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
